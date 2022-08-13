c7a0c007dcd3165f7467af4cc4e6197cfda4bc42
ShehabAdel99 <96583491+ShehabAdel99@users.noreply.github.com>
2022-08-13 14:25:23 +0200

    .

--- Implement your searching algorithm/Implement your searching algorithm/Algorithm.c
+++ Implement your searching algorithm/Implement your searching algorithm/Algorithm.c
@@ -28,34 +28,6 @@ void quicksort(int number[], int first, int last) {
     }
 }
 
-// A recursive binary search function. It returns
-// location of x in given array arr[l..r] is present,
-// otherwise -1
-//int binarySearch(int arr[], int x , int l, int r)
-//{
-//    if (r >= l) {
-//        int mid = l + (r - l) / 2;
-//
-//        // If the element is present at the middle
-//        // itself
-//        if (arr[mid] == x)
-//            return mid;
-//
-//        // If element is smaller than mid, then
-//        // it can only be present in left subarray
-//        if (arr[mid] > x)
-//            return binarySearch(arr, l, mid - 1, x);
-//
-//        // Else the element can only be present
-//        // in right subarray
-//        return binarySearch(arr, mid + 1, r, x);
-//    }
-//
-//    // We reach here when element is not
-//    // present in array
-//    return -1;
-//}
-
 int binarySearch(int array[], int x, int low, int high) {
     // Repeat until the pointers low and high meet each other
     while (low <= high) {
